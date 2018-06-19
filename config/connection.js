068b0fb2e069b16850cdfcd9f75b5389843f3f61
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-06-19 18:42:31 -0400

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,7 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+var mongoose = require("mongoose");
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+var connection = mongoose.connect("mongodb://localhost/JoseSchema");
 // Export connection for our ORM to use.
 module.exports = connection;
+
--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,6 @@ function printQuestionMarks(num) {
 
   return arr.toString();
 }
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
@@ -38,7 +37,6 @@ function objToSql(ob) {
   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
-
 // Object for all our SQL statement functions.
 var orm = {
   all: function(tableInput, cb) {
@@ -49,59 +47,7 @@ var orm = {
       }
       cb(result);
     });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
   }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
