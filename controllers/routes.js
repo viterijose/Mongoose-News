777f4564f79caa642001cb32262750719d9bbb21
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-06-21 15:19:17 -0400

    updates on handlebars

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,10 +6,11 @@ var router = express.Router();
 var scrape = require("../models/scrape.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get("/scrape", function(req, res) {
   // res.send("Test");
   scrape(function(data){
-    res.send(data);
+    console.log(data);
+    res.render("index", {data});
   });
   // articles.all(function(data) {
   //   var hbsObject = {
