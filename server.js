559a5d3438eee5a6c3436a6d8bb7721d95db13e1
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-06-21 16:55:00 -0400

    fixed bug

--- server.js
+++ server.js
@@ -19,70 +19,6 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-<<<<<<< HEAD
-mongoose.connect("mongodb://localhost/JoseSchema");
-
-app.get("/scrape", function (req, res) {
-  axios.get("https://www.nytimes.com/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h2").each(function (i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children().text();
-      console.log(result.title);
-      result.link = $(element).children().attr("href");
-      console.log(result.link);
-
-      // Create a new Article using the `result` object built from scraping
-      db.create(result)
-        .then(function (dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
-    });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
-  });
-});
-
-// // Create an object containing dummy data to save to the database
-// var data = {
-//   skills: ["item1", "item2", "item3"],
-//   boolean: false,
-//   lastname:
-//     "Ferguson",
-//   age: 42,
-//   name: "Kevin"
-// };
-
-// // Save a new Example using the data object
-// kevModel.create(data)
-//   .then(function (dbExample) {
-//     // If saved successfully, print the new Example document to the console
-//     console.log(dbExample);
-//   })
-//   .catch(function (err) {
-//     // If an error occurs, log the error message
-//     console.log(err.message);
-//   });
-
-
-app.listen(PORT, function () {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
-=======
 // Import routes and give the server access to them.
 var routes = require("./controllers/routes.js");
 
@@ -121,4 +57,4 @@ app.listen(PORT, function() {
 //   });
 
 // });
->>>>>>> 8efc56aa90e600ea2b43921fd7d6ca76ee0c778b
+
