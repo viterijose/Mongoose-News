20670b6f192acb0704b922552f6a2ba088f1310f
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-06-21 16:40:38 -0400

    ignore changes

--- server.js
+++ server.js
@@ -26,31 +26,29 @@ app.set("view engine", "handlebars");
 
 mongoose.connect("mongodb://localhost/JoseSchema");
 
-app.get("/scrape", function (req, res){
-  axios.get("https://www.nytimes.com/").then(function(response) {
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.nytimes.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(this).children().text();
+      console.log(result.title);
+      result.link = $(element).children().attr("href");
+      console.log(result.link);
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
+      db.create(result)
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, send it to the client
           return res.json(err);
         });
@@ -61,30 +59,29 @@ app.get("/scrape", function (req, res){
   });
 });
 
-// Create an object containing dummy data to save to the database
-var data = {
-  skills: ["item1", "item2", "item3"],
-  boolean: false,
-  lastname:
-    "Ferguson",
-  age: 42,
-  name:"Kevin"
-};
+// // Create an object containing dummy data to save to the database
+// var data = {
+//   skills: ["item1", "item2", "item3"],
+//   boolean: false,
+//   lastname:
+//     "Ferguson",
+//   age: 42,
+//   name: "Kevin"
+// };
 
-// Save a new Example using the data object
-kevModel.create(data)
-  .then(function(dbExample) {
-    // If saved successfully, print the new Example document to the console
-    console.log(dbExample);
-  })
-  .catch(function(err) {
-    // If an error occurs, log the error message
-    console.log(err.message);
-  });
+// // Save a new Example using the data object
+// kevModel.create(data)
+//   .then(function (dbExample) {
+//     // If saved successfully, print the new Example document to the console
+//     console.log(dbExample);
+//   })
+//   .catch(function (err) {
+//     // If an error occurs, log the error message
+//     console.log(err.message);
+//   });
 
 
-  app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
