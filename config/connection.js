8f056f3c5166357e1beb527dfa68e1a90cfb1462
Jose Viteri <Jose@Joses-MBP.home>
2018-06-24 10:09:31 -0400

    added mongo

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 
 var mongoose = require("mongoose");
 
-var connection = mongoose.connect("mongodb://localhost/ArticlesSchema");
+// var connection = mongoose.connect("mongodb://localhost/ArticlesSchema");
+var connection = process.env.MONGODB_URI || "mongodb://localhost/ArticlesSchema";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(connection);
 // Export connection for our ORM to use.
 module.exports = connection;
 
